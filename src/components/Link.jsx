a0e3de24c2e49ecded2583d6e20dc94cd871ad72
Sreeram <rachasreeram@gmail.com>
2022-09-02 13:48:42 +0530

    Changed resume URL

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -3,8 +3,8 @@ export default function Link() {
 		<div className="link-to-resume">
 			<p>
 				Link to this resume:
-				<a href="https://sreeramracha.github.io/Resume-2.0/">
-					sreeramracha.github.io/Resume
+				<a href="https://sreeram-resume.herokuapp.com/">
+					sreeram-resume.herokuapp.com
 				</a>
 			</p>
 		</div>
